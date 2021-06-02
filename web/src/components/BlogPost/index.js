a1ca038659c0166c104c09cd42f537930e7683f2
Tomas-Lawton <57382467+Tomas-Lawton@users.noreply.github.com>
2021-06-03 00:23:13 +1000

    sleep 2

--- web/src/components/BlogPost/index.js
+++ web/src/components/BlogPost/index.js
@@ -29,54 +29,27 @@ const BlogPostPreview = (props) => (
               )}
             </div>
           </div>
-        </div>
-
-        <div className={styles.toEnd}>
-          {window.innerWidth > 858 && (
-            <>
-              <ArrowLink
-                to={getBlogUrl(props.publishedAt, props.slug.current)}
-              />
-              <div className={styles.rectBox}>
-                <Fixer>
-                  <BigNumber>0{props.index + 1}</BigNumber>
-                </Fixer>
-                <hr className={styles.rainbowBar} />
-                <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                  {props.categories.map((cat, index) => (
-                    <p
-                      key={index}
-                      style={{ marginLeft: "4vw", marginBottom: "0" }}
-                    >
-                      {cat.title}
-                    </p>
-                  ))}
-                </div>
+          <div className={styles.right}>
+            <div className={styles.leadMediaThumb}>
+              {props.mainImage && props.mainImage.asset && (
+                <img
+                  src={imageUrlFor(buildImageObj(props.mainImage))
+                    .width(600)
+                    .height(Math.floor((9 / 16) * 600))
+                    .auto("format")
+                    .url()}
+                  alt={props.mainImage.alt}
+                />
+              )}
+            </div>
+            {window.innerWidth < 858 && (
+              <div className={styles.date}>
+                {format(new Date(props.publishedAt), "MMMM Mo, yyyy")}
               </div>
-            </>
-          )}
-        </div>
-        <div className={styles.right}>
-          <div className={styles.leadMediaThumb}>
-            {props.mainImage && props.mainImage.asset && (
-              <img
-                src={imageUrlFor(buildImageObj(props.mainImage))
-                  .width(600)
-                  .height(Math.floor((9 / 16) * 600))
-                  .auto("format")
-                  .url()}
-                alt={props.mainImage.alt}
-              />
             )}
           </div>
-          {window.innerWidth < 858 && (
-            <div className={styles.date}>
-              {format(new Date(props.publishedAt), "MMMM Mo, yyyy")}
-            </div>
-          )}
         </div>
       </div>
-
       <div className={styles.toEnd}>
         {window.innerWidth > 858 && (
           <ArrowLink to={getBlogUrl(props.publishedAt, props.slug.current)} />
